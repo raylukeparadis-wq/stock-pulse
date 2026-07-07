266d08c824b40a455d6cabe58bec036b7d77af8a
Ray Luke Paradis <raylukeparadis@gmail.com>
2026-07-07 17:10:12 -0400

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import StockAnalyzer from './StockAnalyzer'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StockAnalyzer />
+  </React.StrictMode>
+)
